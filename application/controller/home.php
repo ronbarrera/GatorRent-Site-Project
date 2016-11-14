afe9a8d14c885d6e10eefc7476ac6c30270d8f43
Ronald Alexander Barrera <rbarrer2@mail.sfsu.edu>
2016-11-13 21:32:30 -0800

    Added the pages: about us and sign up to the home class. Create a location variable to help the navbar for current location. Delete unnecessary files.

--- application/controller/home.php
+++ application/controller/home.php
@@ -15,15 +15,44 @@ class Home extends Controller
      * PAGE: index
      * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
      */
+    
+    /**
+     *
+     * $location helps to active the current location in the navbar.
+     */
+    private $location = "";
+    
+       
+    public function getCurrentLocation(){
+        return $this->location;
+    }
+    
     public function index()
     {
         $search_options = $this->model->getSearchOptions();
-
+        $location = "home";
         require APP . 'view/_templates/header.php';
         require APP . 'view/home/index.php';
         require APP . 'view/_templates/footer.php';
     }
+    
+    public function aboutus()
+    {
+        $search_options = $this->model->getSearchOptions();
+        $location = "aboutus";
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/home/aboutus.php';
+        require APP . 'view/_templates/footer.php';
+    }
 
+    public function signup()
+    {
+        $search_options = $this->model->getSearchOptions();
+        $location = "signup";
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/home/signup.php';
+        require APP . 'view/_templates/footer.php';
+    }
     /**
      * PAGE: search
      * This method handles getting listings from the database that match the search criteria
@@ -31,7 +60,7 @@ class Home extends Controller
     public function search()
     {
         $search_options = $this->model->getSearchOptions();
-
+        
         if (isset($_POST['submit_search'])) {
             $results = $this->model->search($_POST['search_option'], $_POST['search_query']);
         } else {
@@ -39,7 +68,6 @@ class Home extends Controller
         }
 
         require APP . 'view/_templates/header.php';
-        require APP . 'view/home/index.php';
         require APP . 'view/home/results.php';
         require APP . 'view/_templates/footer.php';
     }
