541f0fa2f0dd48c75ff1fa7315dc904f61958792
Ronald Alexander Barrera <rbarrer2@mail.sfsu.edu>
2016-11-14 11:53:35 -0800

    Added new results page. Added results page css file.

--- application/view/home/results.php
+++ application/view/home/results.php
@@ -1,68 +1,63 @@
-<div class="container">
-
-    <?php if (count($results) < 1) { ?>
-        <h5>No Result Found</h5>
+<?php if (count($results) < 1) { ?>
+    <h5>No Result Found</h5>
     <?php
-    } else {
-        $totalresult = count($results);
-        $searchby = $_POST['search_query'];
-        echo "<h5>$totalresult results for \"$searchby\"</h5>";
+} else {
+    ?>
+    <div class="container-fluid" style="background-color:#e8e8e8">
+        <div class="container container-pad" id="property-listings">
 
-        foreach ($results as $result) {
-            ?>
-            <div class="row single-row-listing">
-                <div class="col-sm-10 col-sm-offset-1">
-                    <div class="col-sm-6">
-                        <center><img src="<?php if (isset($result->apartment_id) && isset($result->picture_1)) { echo 'http://sfsuswe.com/~f16g13/imgfs/' . htmlspecialchars($result->apartment_id) . "/" . htmlspecialchars($result->picture_1); } else { echo ''; }?>" style="height:200px; margin-left:-15px;" /></center>
-                    </div>
-                     
-                    <div class="col-sm-6">  
-                            <div class="container">
-                                <h4>Price: <?php if (isset($result->price)) {
-                echo htmlspecialchars($result->price, ENT_QUOTES, "UTF-8");
-            } ?></h4>
-                                <h4>Bedrooms: <?php if (isset($result->rooms)) {
-                echo htmlspecialchars($result->rooms, ENT_QUOTES, "UTF-8");
-            } ?></h4>
-                                <h4>Zip Code: <?php if (isset($result->zipcode)) {
-                echo htmlspecialchars($result->zipcode, ENT_QUOTES, "UTF-8");
-            } ?></h4>
-                                <button type="submit" class="btn btn-primary btn-lg">View</button>
-                                <button type="submit" class="btn btn-primary btn-lg">Contact</button>
-                            </div>
-
-                    </div>          
-                    </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Apartment Listings</h1>
+                    <p><?php echo count($results) ?> results for "<?php echo $_POST['search_query'] ?>"</p>
                 </div>
-            
+            </div>
+            <div class="row">
+                <?php foreach ($results as $result) {
+                    ?>
 
-    <?php }} ?>
+                <div class="col-sm-6">
+                    <!-- Begin Listing -->
+                        <div class="brdr bgc-fff pad-10 box-shad btm-mrg-20 property-listing">
+                            <div class="media">
+                                
+                                 <a class="pull-left" href="#" target="_parent">
+                                     <img alt="image" class="img-thumbnail-listing" src="<?php if (isset($result->apartment_id) && isset($result->picture_1)) { echo 'http://sfsuswe.com/~f16g13/imgfs/' . htmlspecialchars($result->apartment_id) . "/" . htmlspecialchars($result->picture_1); } else { echo ''; }?>" width="180px" height="135px"></a>
 
-</div>
+                            
+                                <div class="clearfix visible-sm"></div>
 
-<script type="text/javascript">
-    /* This JS selects the image links from the results table
-     * and creates a thumbnail image that links to the original
-     */
-    $(function () {
-        var link;
+                                <div class="media-body fnt-smaller">
+                                    <a href="#" target="_parent"></a>
 
-        // results is a jQuery object containing DOM elements,
-        // so iterate through each one and extract the href from the
-        // anchor element and set it as the src for an image element.
-        $('#results').children().each(function (index, row) {
-            $(row).children().each(function (index, cell) {
-                if (cell.id) {
-                    link = $(cell).children().first();
-                    if (link && link.attr('href').length > 0) {
-                        link.html('<img src="'.concat(link.attr('href'), '" width="320px">'));
-                    } else {
-                        // If no image is found, remove the anchor element from the table cell
-                        link.remove();
-                    }
-                }
-            });
-        });
-    });
-</script>
+                                    <h4 class="media-heading">
+                                        <a href="#" target="_parent">$<?php if (isset($result->price)) {echo htmlspecialchars($result->price, ENT_QUOTES, "UTF-8");} ?> <small class="pull-right"><?php if (isset($result->zipcode)) {echo htmlspecialchars($result->zipcode, ENT_QUOTES, "UTF-8");} ?></small></a></h4>
 
+
+                                    <ul class="list-inline mrg-0 btm-mrg-10 clr-535353">
+                                        <li>SqFt</li>
+                                        <li style="list-style: none">|</li>
+                                        <li>Baths</li>
+                                        <li style="list-style: none">|</li>
+                                        <li><?php if (isset($result->rooms)) {echo htmlspecialchars($result->rooms, ENT_QUOTES, "UTF-8");} ?> Baths</li>
+                                    </ul>
+                                    
+                                    <button type="submit" class="btn btn-primary btn-lg">View</button>
+                                    <button type="submit" class="btn btn-primary btn-lg">Contact</button>
+                                    
+                                </div>
+                            </div>
+                        </div><!-- End Listing-->
+                </div>
+                        
+                  
+                
+                
+                <?php } ?>      
+            </div><!-- End row -->
+        </div><!-- End container -->
+    </div>
+<?php } ?>
+
+    
+ 
