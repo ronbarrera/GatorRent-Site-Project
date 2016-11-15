60849a2db1ceb26402b62d14378796242bc45dfb
Steven Heng Wong <swong7@mail.sfsu.edu>
2016-11-15 01:05:24 +0000

    indentation fixed

--- application/model/userModel.php
+++ application/model/userModel.php
@@ -1,45 +1,48 @@
 <?php
+
 class UserModel {
-		function __construct($db) {
-			try {
-				$this->db = $db;
-			} catch (PDOException $e) {
-				exit('Database connection could not be established.');
-			}
-		}
 
-		public function register($username, $password, $email) {
-			$sql = "INSERT INTO user (username, password, email) VALUES (:username, :password, :email)";
-			$query = $this->db->prepare($sql);
+    function __construct($db) {
+        try {
+            $this->db = $db;
+        } catch (PDOException $e) {
+            exit('Database connection could not be established.');
+        }
+    }
 
-			$query->bind_param(':username', $username);
-			$query->bind_param(':password', hash('sha256', $password));
-			$query->bind-param(':email', $email);
+    public function register($username, $password, $email) {
+        $sql = "INSERT INTO user (username, password, email) VALUES (:username, :password, :email)";
+        $query = $this->db->prepare($sql);
 
-			$query->execute();
-		}
+        $query->bind_param(':username', $username);
+        $query->bind_param(':password', hash('sha256', $password));
+        $query->bind - param(':email', $email);
 
-		public function login($username, $password) {
-			$sql = "SELECT * FROM user WHERE username=:username AND password=:password ;";
-			$query = $this->db->prepare($sql);
+        $query->execute();
+    }
 
-			$query->bindParam(':username', $username);
-			$query->bindParam(':password', hash('sha256', $password));
+    public function login($username, $password) {
+        $sql = "SELECT * FROM user WHERE username=:username AND password=:password ;";
+        $query = $this->db->prepare($sql);
 
-			$query->execute();
-      $result = $query->fetchAll();
-      print_r($result);
-      //$value = $result == 1;
-      //print($value);
-      
-			// checks if username and password are the same
-			if(!$result) {
-				// return false, lets view output error message
-        echo "Error, username or password does not match";
-			} else {
-				// return true
-        echo "Success!";
-			}
-		}
+        $query->bindParam(':username', $username);
+        $query->bindParam(':password', hash('sha256', $password));
+
+        $query->execute();
+        $result = $query->fetchAll();
+        print_r($result);
+        //$value = $result == 1;
+        //print($value);
+        // checks if username and password are the same
+        if (!$result) {
+            // return false, lets view output error message
+            echo "Error, username or password does not match";
+        } else {
+            // return true
+            echo "Success!";
+        }
+    }
+
 }
+
 ?>
