57fb410cc9d2ad84963139fd87789babd28b03ed
Arjun Anand Patel <apatel3@mail.sfsu.edu>
2016-12-06 03:50:52 +0000

    is_available is now a boolean in theory and there is a baths field just like room

--- _install/createTables.sql
+++ _install/createTables.sql
@@ -31,6 +31,7 @@ CREATE TABLE IF NOT EXISTS `f16g13`.`Apartments`(
    `zipcode`int (5),
    `price`int (5),
    `rooms`int (1),
+   `baths`int (1),
    `description` varchar(512) COLLATE utf8_unicode_ci,
    `created_date` datetime DEFAULT CURRENT_TIMESTAMP COLLATE utf8_unicode_ci,
    `modified_date` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COLLATE utf8_unicode_ci,
@@ -38,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `f16g13`.`Apartments`(
    `picture_2` varchar(200) COLLATE utf8_unicode_ci,
    `picture_3` varchar(200) COLLATE utf8_unicode_ci,
    `picture_4` varchar(200) COLLATE utf8_unicode_ci,
-   `is_available` tinyint(1),
+   `is_available` boolean DEFAULT true,
    PRIMARY KEY (`apartment_id`),
    FOREIGN KEY(`lessor_id`) REFERENCES `f16g13`.`Lessors`(`lessor_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
