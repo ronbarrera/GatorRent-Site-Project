35d8b5a431246acfc9f6860a44f9397de210c9d6
Samuel Joseph Candito <scandito@mail.sfsu.edu>
2016-12-14 23:26:41 -0800

    displays errors on registration

--- application/view/register/index.php
+++ application/view/register/index.php
@@ -5,7 +5,7 @@
         <div class="col-sm-4">
             <form class="form-horizontal" action='<?php echo URL ?>register/registerUser' method="POST">
                 <fieldset>
-                    <br>
+                    <br />
                     <div id="legend">
                         <legend class="">Create Account</legend>
                     </div>
@@ -14,8 +14,15 @@
                         <label class="control-label">Full Name*</label>
                         <div class="controls">
                             <input type="text" id="firstName" name="firstName" placeholder="First Name" class="form-control" value="<?php if (isset($currentForm['firstName'])) { echo htmlspecialchars($currentForm['firstName']); } ?>">
-                            <br>
+                            <?php if (isset($formErrors['firstName'])) { ?>
+                                <span style="color:red"><?php echo $formErrors['firstName']; ?></span>
+                                <br />
+                            <?php } ?>
+                            <br />
                             <input type="text" id="lastName" name="lastName" placeholder="Last Name" class="form-control" value="<?php if (isset($currentForm['firstName'])) { echo htmlspecialchars($currentForm['lastName']); } ?>">
+                            <?php if (isset($formErrors['lastName'])) { ?>
+                                <span style="color:red"><?php echo $formErrors['lastName']; ?></span>
+                            <?php } ?>
                         </div>
                     </div>
                     <!-- User type -->
@@ -26,12 +33,18 @@
                             <option value="Renter" <?php if (isset($currentForm['userType']) && $currentForm['userType'] === 'Renter') { echo htmlspecialchars('selected'); } ?>>Renter</option>
                             <option value="Lessor" <?php if (isset($currentForm['userType']) && $currentForm['userType'] === 'Lessor') { echo htmlspecialchars('selected'); } ?>>Lessor</option>
                         </select>
+                        <?php if (isset($formErrors['userType'])) { ?>
+                            <span style="color:red"><?php echo $formErrors['userType']; ?></span>
+                        <?php } ?>
                     </div>
                     <div class="form-group">
                         <!-- Renter ID -->
                         <label class="control-label" for="renterId">Student ID</label>
                         <div class="controls">
                             <input type="text" id="renterId" name="renterId" placeholder="Please provide your student ID (required for renters)" class="form-control">
+                            <?php if (isset($formErrors['renterId'])) { ?>
+                                <span style="color:red"><?php echo $formErrors['renterId']; ?></span>
+                            <?php } ?>
                         </div>
                     </div>
                     <div class="form-group">
@@ -39,6 +52,9 @@
                         <label class="control-label" for="email">E-mail*</label>
                         <div class="controls">
                             <input type="text" id="email" name="email" placeholder="Please provide your E-mail" class="form-control" value="<?php if (isset($currentForm['email'])) { echo htmlspecialchars($currentForm['email']); } ?>">
+                            <?php if (isset($formErrors['email'])) { ?>
+                                <span style="color:red"><?php echo $formErrors['email']; ?></span>
+                            <?php } ?>
                         </div>
                     </div>
                     <div class="form-group">
@@ -46,6 +62,9 @@
                         <label class="control-label" for="password">Password*</label>
                         <div class="controls">
                             <input type="password" id="password" name="password" placeholder="Password should be at least 4 characters" class="form-control">
+                            <?php if (isset($formErrors['password'])) { ?>
+                                <span style="color:red"><?php echo $formErrors['password']; ?></span>
+                            <?php } ?>
                         </div>
                     </div>
                     <div class="form-group">
@@ -54,16 +73,22 @@
                         <div class="controls">
                             <input type="password" id="passwordVerify" name="passwordVerify" placeholder="Verify password" class="form-control">
                         </div>
+                        <?php if (isset($formErrors['passwordVerify'])) { ?>
+                            <span style="color:red"><?php echo $formErrors['passwordVerify']; ?></span>
+                        <?php } ?>
                     </div>
-                    <br>
+                    <br />
                     <!-- Conditions of use and Privacy notice agreement. -->
                     <div class="checkbox">
                         <label>
                             <input type="checkbox" id="tosComply" name="tosComply">
                             I agree to GatorRent's <a href="<?php echo URL; ?>home/conditionsofuse" target="_blank">Conditions of Use</a> and <a href="<?php echo URL; ?>home/privacynotice" target="_blank">Privacy Notice</a>.
                         </label>
+                        <?php if (isset($formErrors['tosComply'])) { ?>
+                            <span style="color:red"><?php echo $formErrors['tosComply']; ?></span>
+                        <?php } ?>
                     </div>
-                    <br>
+                    <br />
                     <div class="form-group">
                         <!-- Create account button -->
                         <div class="controls">
